41845f7580d75713345a4cc6ccc23195318e2396
JitTechPlace <jrr.workplace@gmail.com>
2023-05-28 17:52:04 +0400

    MinorChanges

---
+++ LoginControl/Logic/ServerHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoginControl.Logic;
+
+namespace LoginControl.Logic
+{
+   public class EmailHelpers
+    {
+        //public static async Task<bool> IsEmailRegistered(string EmailId)
+        //{
+        //    //return await TNCSAPI.Instance.IsEmailRegistered(EmailId);
+        //}
+    }
+}
