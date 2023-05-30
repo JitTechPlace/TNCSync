059e3321f2b1334af14c56ec2d538a23365c1189
JitTechPlace <jrr.workplace@gmail.com>
2023-05-30 13:05:42 +0400

    Changes in UI for Base Design

---
+++ TNCSync/BaseControls/VendorsList.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+////using TNCSync.Class.QuickBookData;
+namespace TNCSync.BaseControls
+{
+    /// <summary>
+    /// Interaction logic for VendorsList.xaml
+    /// </summary>
+    public partial class VendorsList : UserControl
+    {
+        public VendorsList()
+        {
+            InitializeComponent();
+        }
+
+        private void syncVendorList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
